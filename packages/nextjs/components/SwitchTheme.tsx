bfe7d3b3c882c450f894903b8c74b3cb53daab32
jkwamlah <jkwamlah@outlook.com>
2023-09-24 06:34:14 +0000

    made prettier changes

--- packages/nextjs/components/SwitchTheme.tsx
+++ packages/nextjs/components/SwitchTheme.tsx
@@ -13,20 +13,22 @@ export const SwitchTheme = ({ className }: { className?: string }) => {
   }, [isDarkMode]);
 
   return (
-      <div className={`d-flex align-items-center ${className}`}>
-          <div className="form-check form-switch form-switch-md mb-3" dir="ltr">
-              <input className="form-check-input bg-primary" type="checkbox" id="theme-toggle"
-                     onChange={toggle}
-                     checked={isDarkMode}
-              />
-              {isMounted() && (
-                  <label htmlFor="theme-toggle" className={`toggle-switch ${isDarkMode ? "toggle-switch-active" : ""}`}>
-                      <SunIcon className="h-50 w-5" />
-                      <MoonIcon className="h-50 w-5" />
-                  </label>
-              )}
-          </div>
-
+    <div className={`d-flex align-items-center ${className}`}>
+      <div className="form-check form-switch form-switch-md mb-3" dir="ltr">
+        <input
+          className="form-check-input bg-primary"
+          type="checkbox"
+          id="theme-toggle"
+          onChange={toggle}
+          checked={isDarkMode}
+        />
+        {isMounted() && (
+          <label htmlFor="theme-toggle" className={`toggle-switch ${isDarkMode ? "toggle-switch-active" : ""}`}>
+            <SunIcon className="h-50 w-5" />
+            <MoonIcon className="h-50 w-5" />
+          </label>
+        )}
       </div>
+    </div>
   );
 };
